af1431f1b55f7baea90dc2554cbb81e8af95bb3f
Jack LeVasseur <159185068+Jack-LeVasseur@users.noreply.github.com>
2024-05-16 23:13:32 +0000

    Updated index.html

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  // input
+  const age = parseInt(document.getElementById('age-entered').value)
+  const dayTuesdayChecked = document.getElementById('option-tuesday').checked
+  const dayThursdayChecked = document.getElementById('option-thursday').checked
+
+  // process
+  if ((dayTuesdayChecked == true || dayThursdayChecked == true) || (age > 12 && age < 21)) {
+    document.getElementById('answer').innerHTML = "You get the student discount"
+  } else {
+    document.getElementById('answer').innerHTML = "You must pay regular price"
+  }
+}
